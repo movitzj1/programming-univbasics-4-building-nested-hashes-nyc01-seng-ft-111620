f0c3bcdaa15435039e11de4a44489aa05bc8482c
Justin Movitz <movitzj1@montclair.edu>
2020-10-23 17:56:24 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   monopoly =
   {
-    :railroads =>
+    railroads:
     {
 
     }
@@ -11,14 +11,14 @@ end
 def monopoly_with_second_tier
   monopoly =
   {
-    :railroads =>
+    railroads:
     {
-      :pieces => 4,
-      :rent_in_dollars =>
+      pieces: 4,
+      rent_in_dollars:
       {
 
       },
-      :names =>
+      names:
       {
 
       }
@@ -29,31 +29,31 @@ end
 def monopoly_with_third_tier
   monopoly =
   {
-    :railroads =>
+    railroads:
     {
-      :pieces => 4,
-      :rent_in_dollars =>
+      pieces: 4,
+      rent_in_dollars:
       {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
       },
-      :names =>
+      names:
       {
-        :reading_railroad=>
+        reading_railroad:
         {
 
         },
-        :pennsylvania_railroad=>
+        pennsylvania_railroad:
         {
 
         },
-        :b_and_o_railroad=>
+        b_and_o_railroad:
         {
 
         },
-        :shortline_railroad=>
+        shortline_railroad:
         {
 
         }
@@ -65,33 +65,33 @@ end
 def monopoly_with_fourth_tier
   monopoly =
   {
-    :railroads =>
+    railroads:
     {
-      :pieces => 4,
-      :rent_in_dollars =>
+      pieces: 4,
+      rent_in_dollars:
       {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
+        one_piece_owned: 25,
+        two_pieces_owned: 50,
+        three_pieces_owned: 100,
+        four_pieces_owned: 200
       },
-      :names =>
+      names:
       {
-        :reading_railroad=>
+        reading_railroad:
         {
-          :mortgage_value => 100
+          mortgage_value: 100
         },
-        :pennsylvania_railroad=>
+        pennsylvania_railroad:
         {
-          :mortgage_value => 200
+          mortgage_value: 200
         },
-        :b_and_o_railroad=>
+        b_and_o_railroad:
         {
-          :mortgage_value => 400
+          mortgage_value: 400
         },
-        :shortline_railroad=>
+        shortline_railroad:
         {
-          :mortgage_value => 800
+          mortgage_value: 800
         }
       }
     }
